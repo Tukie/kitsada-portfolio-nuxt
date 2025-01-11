4915c34fdf040fda392f4cd6b23ca1682fec7d07
Tukie <sadawutsunee.2885@gmail.com>
2025-01-11 21:54:17 +0700

    update: changed from docker to hono.js and removed chart

--- components/Skills.vue
+++ components/Skills.vue
@@ -1,42 +1,4 @@
 <script lang="js" setup>
-
-onMounted(() => {
-    chartData.value = setChartData();
-    chartOptions.value = setChartOptions();
-});
-
-const chartData = ref();
-const chartOptions = ref();
-
-const setChartData = () => {
-    return {
-        labels: ['PHP', 'Vue.js', 'Nuxt.js', 'MySQL', 'Docker', 'Git', 'Bootstrap', 'Tailwind', 'Leaf PHP (Framework)'],
-        datasets: [
-            {
-                data: [40, 17, 3, 10, 5, 5, 15, 2, 3],
-                backgroundColor: ["#767CBA", "#43B684", "#00DF84", "#2989CD", "#7EACB5", "#000000", "#37BEF7", "#C96868", "#624E88"],
-            }
-        ]
-    };
-};
-
-const setChartOptions = () => {
-    return {
-        plugins: {
-            legend: {
-                display: false
-            },
-            datalabels: {
-                display: true,
-                formatter: (value) => {
-                    console.log(value);
-                    return value + '%';
-                },
-            },
-        }
-    };
-};
-
 const skillLists = ref([
     {
         name: "Bootstrap",
@@ -69,9 +31,9 @@ const skillLists = ref([
         level: "advanced"
     },
     {
-        name: "Docker",
-        icon: "docker.webp",
-        href: "https://www.docker.com/",
+        name: "Hono.js",
+        icon: "hono.png",
+        href: "https://hono.dev/",
         level: "beginner"
     },
     {
@@ -124,14 +86,7 @@ const openLink = (url) => {
         </div>
         <div class="bg-card p-5 rounded-4" data-aos="fade-up" data-aos-delay="100">
             <div class="row g-4">
-                <div class="col-xl-4 h-100" data-aos="fade-right" data-aos-delay="400">
-                    <div class="h-100 py-5">
-                        <div class="d-flex justify-content-center align-items-center">
-                            <Chart type="doughnut" :data="chartData" :options="chartOptions" />
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-8 h-100" data-aos="fade-left" data-aos-delay="400">
+                <div class="col-12 h-100" data-aos="fade-up" data-aos-delay="400">
                     <div class="d-flex flex-column justify-content-center align-items-center h-100">
                         <div class="row g-4">
                             <div class="col-md-4 h-100" v-for="skill in skillLists" :key="skill.name">
